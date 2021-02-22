d8e61e7b9dcb457fe87247bc2a8f0db3018128cc
Casper Kristiansson <36133918+kab2k@users.noreply.github.com>
2021-02-22 22:32:33 +0100

    Update twitchApi.py

--- twitchApi.py
+++ twitchApi.py
@@ -4,6 +4,7 @@ import requests
 import os
 import shutil
 import time
+import sys
 
 import argparse
 import http.client
@@ -20,8 +21,20 @@ from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
+displayNameTopChannel = []
+displayNameLinkTopChannel = []
+viewsTopChannel = []
+clipCreatorNameTopChannel = []
+clipCreatorLinkTopChannel = []
+clipUploadTimeTopChannel = []
+currentFileTopChannel = []
 
-API_ENDPOINT = 'https://api.twitch.tv/kraken/clips/top?channel=Cyberpunk%202077&period=all&trending=false&limit=100'
+with open('currentFileTopChannel.py', 'w') as filehandle:
+    for listitem in currentFileTopChannel:
+        filehandle.write('%s\n' % listitem)
+time.sleep(15)
+
+API_ENDPOINT = 'https://api.twitch.tv/kraken/clips/top?channel=Sykkuno&period=all&trending=false&limit=100'
 ID = ''
 auth = 'application/vnd.twitchtv.v5+json'
 
@@ -32,5 +45,64 @@ head = {
 
 r = requests.get(url = API_ENDPOINT, headers = head)
 
-twitchClipLinksLOL = []
+twitchClipLinksTopChannel = []
 data = r.json()
+
+for link in data['clips']:
+    store = str(link['url'])
+    twitchClipLinksTopChannel.append(store)
+
+for link in data['clips']:
+    store = str(link['broadcaster']['display_name'])
+    displayNameTopChannel.append(store)
+
+for link in data['clips']:
+    store = str(link['broadcaster']['channel_url'])
+    displayNameLinkTopChannel.append(store)
+
+for link in data['clips']:
+    store = str(link['views'])
+    viewsTopChannel.append(store)
+
+for link in data['clips']:
+    store = str(link['curator']['display_name'])
+    clipCreatorNameTopChannel.append(store)
+
+for link in data['clips']:
+    store = str(link['curator']['channel_url'])
+    clipCreatorLinkTopChannel.append(store)
+
+for link in data['clips']:
+    store = str(link['created_at'])
+    clipUploadTimeTopChannel.append(store)
+
+
+with open('apiTwitchClipLinksTopChannel.py', 'w') as filehandle:
+    for listitem in twitchClipLinksTopChannel:
+        filehandle.write('%s\n' % listitem)
+
+with open('apiDisplayNameTopChannel.py', 'w') as filehandle:
+    for listitem in displayNameTopChannel:
+        filehandle.write('%s\n' % listitem)
+
+with open('apiDisplayNameLinkTopChannel.py', 'w') as filehandle:
+    for listitem in displayNameLinkTopChannel:
+        filehandle.write('%s\n' % listitem)
+
+with open('apiViewsTopChannel.py', 'w') as filehandle:
+    for listitem in viewsTopChannel:
+        filehandle.write('%s\n' % listitem)
+
+with open('apiClipCreatorNameTopChannel.py', 'w') as filehandle:
+    for listitem in clipCreatorNameTopChannel:
+        filehandle.write('%s\n' % listitem)
+
+with open('apiClipCreatorLinkTopChannel.py', 'w') as filehandle:
+    for listitem in clipCreatorLinkTopChannel:
+        filehandle.write('%s\n' % listitem)
+
+with open('apiClipUploadTimeTopChannel.py', 'w') as filehandle:
+    for listitem in clipUploadTimeTopChannel:
+        filehandle.write('%s\n' % listitem)
+
+print('Urls stored in List')
